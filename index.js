e42a28498bfc1a94e0eedf7f73d51b6962445dcf
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-06 17:58:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,12 +66,12 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber) {
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    
     for (let i = 0; i < cart.length; i++) { 
       
       console.out(cart[i].itemName) }
     
-    
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
   }
   else { return "Sorry, we don't have a credit card on file for you."  }
 }
